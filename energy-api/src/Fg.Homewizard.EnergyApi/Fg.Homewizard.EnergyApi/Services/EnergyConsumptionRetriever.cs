07c6fa726f16f5c9280ca84a9037c448491052ee
Frederik Gheysels <frederik.gheysels@codit.eu>
2024-03-24 10:03:12 +0100

    energy api commit

---
+++ energy-api/src/Fg.Homewizard.EnergyApi/Fg.Homewizard.EnergyApi/Services/EnergyConsumptionRetriever.cs
@@ -0,0 +1,10 @@
+﻿namespace Fg.Homewizard.EnergyApi.Services
+{
+    public class EnergyConsumptionRetriever
+    {
+        public Task GetElectricityConsumptionForPeriod()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
